a8b15963e8ebdaaff25c8ebcf7e9a08856f26dc7
Donny van der Meer <donny@vitroscan.nl>
2024-10-15 11:14:38 +0200

    linting updates

--- inflammation/models.py
+++ inflammation/models.py
@@ -7,41 +7,10 @@ inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 import glob
+import json
 import os
 
 import numpy as np
-import json
-
-
-
-def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array.
-
-    :param data: 2D-numpy array with patients on rows and days on columns
-    :raises AxisError: raised if data is not an array
-    :returns: np.ndarray
-    """
-    return np.mean(data, axis=0)
-
-
-def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array.
-
-    :param data: 2D-numpy array with patients on rows and days on columns
-    :raises AxisError: raised if data is not an array
-    :returns: np.ndarray
-    """
-    return np.max(data, axis=0)
-
-
-def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array.
-
-    :param data: 2D-numpy array with patients on rows and days on columns
-    :raises AxisError: raised if data is not an array
-    :returns: np.ndarray
-    """
-    return np.min(data, axis=0)
 
 class CSVDataSource:
 
@@ -95,6 +64,38 @@ class JSONDataSource:
             raise ValueError(f"No inflammation data JSON files found in path {self.data_dir}")
         data = map(self.load_json, data_file_paths)
         return data
+
+
+def daily_mean(data):
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: 2D-numpy array with patients on rows and days on columns
+    :raises AxisError: raised if data is not an array
+    :returns: np.ndarray
+    """
+    return np.mean(data, axis=0)
+
+
+def daily_max(data):
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: 2D-numpy array with patients on rows and days on columns
+    :raises AxisError: raised if data is not an array
+    :returns: np.ndarray
+    """
+    return np.max(data, axis=0)
+
+
+def daily_min(data):
+    """Calculate the daily min of a 2D inflammation data array.
+
+    :param data: 2D-numpy array with patients on rows and days on columns
+    :raises AxisError: raised if data is not an array
+    :returns: np.ndarray
+    """
+    return np.min(data, axis=0)
+
+
 
 
 def compute_stddev_by_day(data):
